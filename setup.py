0b885edfc3edad7a3100d4fecf5700b6dbb2deb5
Nathan Harrington <nharrington@wasatchphotonics.com>
2015-08-17 10:22:10 -0400

    updated revisions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://github.com/nharringtonwasatch/WasatchUSB',
     'download_url': 'https://github.com/nharringtonwasatch/WasatchUSB',
     'author_email': 'nharrington@wasatchphotonics.com',
-    'version': '0.1',
+    'version': '1.0.0',
     'install_requires': ['phidgeter', 'pyusb', 'numpy'],
     'packages': ['wasatchusb'],
     'scripts': [],
