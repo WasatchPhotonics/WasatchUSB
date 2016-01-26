fde6bed36cb5f468ab57aedd8443c585c4861146
nharringtonwasatch <nharrington@wasatchphotonics.com>
2016-01-26 09:42:49 -0500

    old style open

--- wasatchusb/feature_identification.py
+++ wasatchusb/feature_identification.py
@@ -25,3 +25,68 @@ class ListDevices(object):
 
         return list_devices
 
+class Device(object):
+    def __init__(self, vid=0x24aa, pid=0x1000):
+        log.debug("init")
+        self.vid = vid
+        self.pid = pid
+
+    def connect(self):
+        """ Attempt to connect to the specified device. Log any failures and
+        return None if there is a problem.
+        """
+
+        device = None
+        try:
+            for bus in usb.busses():
+                for dev in bus.devices:
+                    if dev.idVendor == self.vid and  \
+                    dev.idProduct == self.pid:
+                        log.info("attempt open")
+                        device = dev.open()
+
+        except Exception as exc:
+            log.warn("Failure in connect %s", exc)
+            return device
+
+        log.debug("Attempt to set configuration")
+        try:
+            result = device.setConfiguration(1)
+        except Exception as exc:
+            log.warn("Failure in setConfiguration %s", exc)
+
+        try:
+            result = device.claimInterface(0)
+        except Exception as exc:
+            log.warn("Failure in claimInterface: %s", exc)
+
+        return device
+
+    def device_disconnect(self):
+        if self.od <= 0:
+            #self.error_message = "Can't disconnect a non-open device"
+            return 0
+
+        try:
+            #TODO: actually close device
+            result = "fake"
+            #result = self.od.releaseInterface()
+            #self.log.info("released " + str(result) + " " + str( self.od))
+            if result != None:
+                self.error_message = "Cannot close device"
+                return 0
+        except:
+            self.log.warn("releaseInterface call fail: " + \
+                             str(sys.exc_info()))
+            pass
+
+        #self.log.info("out pass ")
+        self.my_vid = -1
+        self.my_pid = -1
+
+        # Expect the release to fail on unplug, if you issue a disconnect above
+        # that fails for different reason that's a ret code 0, otherwise return
+        # a 1
+
+        return 1
+
--- wasatchusb/test/test_feature_identification.py
+++ wasatchusb/test/test_feature_identification.py
@@ -1,8 +1,9 @@
 """ Test cases for the feature_identification.py module that provides 2016-era
-control for wasatch photonics spectrometers and devices.
+control for wasatch photonics spectrometers and dev_list.
 """
 
 import sys
+import pytest
 import logging
 log = logging.getLogger(__name__)
 
@@ -11,18 +12,39 @@ log.addHandler(strm)
 
 
 class TestFeatureIdentification():
-    def test_list_no_devices(self):
-        log.critical("Make sure no devices are connected")
 
+    @pytest.fixture
+    def dev_list(self):
         from wasatchusb import feature_identification
-        devices = feature_identification.ListDevices()
-        result = devices.get_all()
+        dev_list = feature_identification.ListDevices()
+        return dev_list
+
+    def test_list_no_dev_list(self, dev_list):
+        log.critical("Make sure no dev_list are connected")
+
+        result = dev_list.get_all()
         assert len(result) == 0
 
-    def test_list_single_device(self):
+    def test_list_single_device(self, dev_list):
         log.critical("Make sure only one device connected")
 
-        from wasatchusb import feature_identification
-        devices = feature_identification.ListDevices()
-        result = devices.get_all()
+        result = dev_list.get_all()
         assert len(result) == 1
+
+    def test_list_device_is_fx2(self, dev_list):
+        log.critical("Make sure only FX2 0x1000 is connected")
+
+        result = dev_list.get_all()
+        assert len(result) == 1
+        assert result[0][0] == "0x24aa"
+        assert result[0][1] == "0x1000"
+
+    def test_connect_device_close_device(self):
+        log.critical("Expect an FX2 device connected")
+
+        from wasatchusb import feature_identification
+        device = feature_identification.Device()
+        result = device.connect()
+        assert result is not None
+
+
