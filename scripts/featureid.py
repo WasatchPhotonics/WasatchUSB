c0fb9f4a39b4accb63bc5bc7d44ee9b44c1fecb1
Nathan Harrington <nharrington@wasatchphotonics.com>
2016-06-16 14:38:08 -0400

    colored line data

--- scripts/featureid.py
+++ scripts/featureid.py
@@ -10,8 +10,14 @@ strm = logging.StreamHandler(sys.stderr)
 log.addHandler(strm)
 log.setLevel(logging.WARN)
 
+from bashplotlib.scatterplot import plot_scatter
+
 import wasatchusb
 from wasatchusb import feature_identification
+
+import argparse
+from diagram import VerticalBarGraph
+from diagram import Point
 
 def print_device():
     """ Print the default set of data from the device. To diagnose these
@@ -34,7 +40,7 @@ def print_device():
     last_device = dev_list.get_all()[-1]
     last_pid = int(last_device[1], 16)
     print "Connect to last device pid: %s" % last_pid
-    
+
     device = feature_identification.Device(pid=last_pid)
     device.connect()
     print "Model:  %s" % device.get_model_number()
@@ -46,13 +52,80 @@ def print_device():
     print "Laser Avail: %s" % device.get_laser_availability()
     print "Sensor Length: %s" % device.get_sensor_line_length()
 
+    device.set_integration_time(100)
     data = device.get_line()
     avg_data =  sum(data) / len(data)
+
     print ""
     print "Grab Data: %s pixels" % len(data)
     print "Min: %s Max: %s Avg: %s" \
             % (min(data), max(data), avg_data)
 
 
+    with open("xs.txt", "wb") as OUT_FILE:
+        for x_item in range(0, len(data)):
+            OUT_FILE.write("%s\n" % x_item)
+
+    with open("ys.txt", "wb") as OUT_FILE:
+        for y_item in data:
+            OUT_FILE.write("%s\n" % y_item)
+    #print "\n".join(map(str, data))
+    #print "\n".join(map(str, range(0,len(data))))
+    #plot_scatter(f=None, xs="xs.txt", ys="ys.txt", size=20, pch="x",
+            #colour="blue", title="data")
+
+
+    minimum_val = min(data)
+    zero_base_data = []
+    zero_base_data[:] = [x - minimum_val for x in data]
+    #from sparkline import sparkitize
+    #step_size = len(zero_base_data) / 80
+    #print sparkitize(zero_base_data[0::step_size])
+#
+    #print ""
+    #from sparkblocks import spark
+    #print(spark(zero_base_data[0::step_size]))
+#
+    #print ""
+    #one_base_data = []
+    #one_base_data[:] = [x - minimum_val +1 for x in data]
+    #print(spark(one_base_data[0::step_size]))
+#
+    #print ""
+    #from sparkback import *
+    #print_ansi_spark(scale_data(data[0::step_size], ansi_ticks))
+
+
+    parser = argparse.ArgumentParser(
+        description=(
+            'Text mode diagrams using UTF-8 characters and fancy colors.'
+        ),
+        epilog="""fake""",
+    )
+    option = parser.parse_args()
+    option.width = 0
+    option.height = 0
+    option.reverse = False
+    option.input = "ys.txt"
+    try:
+        ostream = sys.stdout.buffer
+    except AttributeError:
+        ostream = sys.stdout
+    option.batch = False
+    option.function = None
+    option.legend = True
+    option.encoding = ''
+    option.color = True
+    option.palette = "default"
+
+    option.size = Point((option.width, option.height))
+
+    engine = VerticalBarGraph(option.size, option)
+
+    istream = open(option.input, 'r')
+    engine.consume(istream, ostream, batch=option.batch)
+
+
 if __name__ == "__main__":
     print_device()
+
